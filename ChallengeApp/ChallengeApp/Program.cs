62cc1e6eadd0a04c87f955cab0d31f9e2aa82b46
kuba.figurski97@gmail.com <kuba.figurski97@gmail.com>
2024-01-06 16:12:06 +0100

    Praca domowa 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "Ewa";
+string gender = "kobieta";
+int age = 30;
+
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (gender == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+
+}
